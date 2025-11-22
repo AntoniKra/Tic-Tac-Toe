1e9ec4452fed3f0f645d6d2bdfa1bc0b30b1407f
Amtoni <dsw52717@student.dsw.edu.pl>
2025-11-22 21:24:56 +0100

    winner_check

--- my-script.js
+++ my-script.js
@@ -20,26 +20,52 @@ const GameController = (function () {
   const player1 = createPlayer("Ricardo", "X");
   const player2 = createPlayer("Cristiano", "O");
   const players = [player1, player2];
+  const winnerCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
   let activePlayer = player1;
 
-  return {
-    switchPlayerTurn: function () {
-      activePlayer = activePlayer === player1 ? player2 : player1;
-      console.log("Kolej gracza:", activePlayer);
-    },
-
-    getActivePlayer: function () {
-      return activePlayer;
-    },
-
-    playRound: function (index) {
-      const board = Gameboard.getBoard();
-
-      if (board[index] === "") {
-        Gameboard.placeMark(index, activePlayer.mark);
-        this.switchPlayerTurn();
-      }
-    },
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === player1 ? player2 : player1;
+    console.log("Kolej gracza:", activePlayer);
   };
+
+  const checkWinner = (board) => {
+    for (let i = 0; i < winnerCombinations.length; i++) {
+      const [a, b, c] = winnerCombinations[i];
+
+      if (board[a] !== "" && board[a] === board[c] && board[a] === board[b]) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const getActivePlayer = () => {
+    return activePlayer;
+  };
+
+  const playRound = (index) => {
+    const board = Gameboard.getBoard();
+
+    if (board[index] === "") {
+      Gameboard.placeMark(index, activePlayer.mark);
+
+      if (checkWinner(board)) {
+        console.log(`wygrałeś: ${activePlayer.name}`);
+        return;
+      }
+      switchPlayerTurn();
+    }
+  };
+
+  return { switchPlayerTurn, getActivePlayer, playRound };
 })();
